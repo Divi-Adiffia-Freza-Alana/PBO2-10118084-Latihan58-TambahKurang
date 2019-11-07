185ecc76257654e313b1605e29d7c78bbfccfb38
DIVI ADIFFIA FREZA ALANA <55723716+Divi-Adiffia-Freza-Alana@users.noreply.github.com>
2019-11-06 21:10:49 -0600

    Add files via upload

---
+++ PBO2-10118084-Latihan58-TambahKurang/src/pbo2/pkg10118084/latihan58/tambahkurang/Bilangan.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118084.latihan58.tambahkurang;
+
+/**
+ *
+ * @author Freza
+ */
+public class Bilangan {
+    int x;
+    int y;
+
+    public Bilangan() {
+       x = 3;
+       y = 4;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+    
+}
---
+++ PBO2-10118084-Latihan58-TambahKurang/src/pbo2/pkg10118084/latihan58/tambahkurang/PBO210118084Latihan58TambahKurang.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118084.latihan58.tambahkurang;
+
+/**
+ *
+*Nama  : Divi Adiffia Freza Alana
+*Kelas : PBO2
+*Nim   : 10118084
+*Deskripsi Program : Program ini berisi program untuk menghitung tambah kurang
+
+ */
+public class PBO210118084Latihan58TambahKurang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JumlahBilangan jumlah = new JumlahBilangan();
+        SelisihBilangan selisih = new SelisihBilangan();
+        
+        jumlah.tampilHasilJumlah();
+        selisih.SelisihBilangan();
+    }
+    
+}
